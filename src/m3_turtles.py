91919969fc946c64ef28dc89346b21a87d41b5be
tarrant <tarranjp@rose-hulman.edu>
2020-06-25 18:16:51 -0400

    m1e and m2

--- src/m3_turtles.py
+++ src/m3_turtles.py
@@ -29,7 +29,7 @@ import rosegraphics as rg
 
 ########################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Allow this file to use the rosegraphics.py file by marking the src
 #   directory as a "Sources Root".  Do that by right clicking on the src folder,
 #   then selector  Mark Directory As --> Sources Root
@@ -73,11 +73,12 @@ matt.speed = 10  # Faster
 matt.backward(50)
 matt.left(90)
 matt.forward(50)
-
+matt.right(120)
+matt.backward(600)
 
 ########################################################################
 #
-# TODO: 3.
+# DONE: 3.
 #   Add a few more line of your own code above to make one of the
 #   existing Turtles move some more and/or have different
 #   characteristics.
@@ -88,10 +89,14 @@ matt.forward(50)
 #   As always, test by running the module.
 #
 ########################################################################
-
+super_turtle = rg.SimpleTurtle()
+super_turtle.pen = rg.Pen('green',50)
+super_turtle.forward(75)
+super_turtle.speed = 50
+super_turtle.left(77)
+super_turtle.forward(96)
 ########################################################################
-#
-# TODO: 4.
+# DONE: 4.
 #   The code above  CONSTRUCTS  two SimpleTurtle objects and gives those objects NAMES:
 #       dave    matt
 #
@@ -112,7 +117,7 @@ matt.forward(50)
 
 ########################################################################
 #
-# TODO: 5.
+# DONE: 5.
 #   Run one more time to be sure that all is still OK.
 #   Ensure that no blue bars on the scrollbar-thing to the right remain.
 #
